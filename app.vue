a52cdef1837ce0bf0574e5c791913861d71f067d
future-html <witsanuporn19646@gmail.com>
2025-02-21 10:30:47 +0700

    add project todolist

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
+<script setup>
+import { useCounterStore } from "#imports";
+
+const store = useCounterStore();
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<h1 class="text-3xl font-bold underline">Hello world! {{ store.count }}</h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+	css: ["~/assets/css/index.css"],
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
+	modules: ["@pinia/nuxt"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
